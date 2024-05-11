bd25c3e252f45aaab3da07bf5943f6d8ca57d097
omuomuMG <omuomunotrice@gmail.com>
2024-05-11 23:47:23 +0900

    add: add-tasks

--- api.ts
+++ api.ts
@@ -1,1 +1,17 @@
-export const getAllTodos = async () => {};
+import { Task } from "./types";
+
+export const getAllTodos = async (): Promise<Task[]> => {
+	const res = await fetch("http://localhost:3000/tasks", { cache: "no-store" }); //no-storeでSSR
+	const todos = res.json();
+	return todos;
+};
+
+export const addTodos = async (todo: Task): Promise<Task[]> => {
+	const res = await fetch("http://localhost:3000/tasks", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(todo),
+	});
+	const newTodo = res.json();
+	return newTodo;
+};
--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+"use client";
+import { addTodos } from "@/api";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 
 function AddTask() {
+	const [taskTitle, setTaskTitle] = useState("");
+	const [id, SetId] = useState<number>(5);
+	const handleSubmit = async (e: FormEvent) => {
+		e.preventDefault();
+		await addTodos({ id: id.toString(), text: taskTitle });
+		setTaskTitle("");
+		SetId(id + 1);
+	};
 	return (
-		<form className="mb-4 space-y-3">
+		<form className="mb-4 space-y-3" onSubmit={handleSubmit}>
 			<input
 				type="text"
 				className="w-full border px-4 py-2 rounded-lg focus:outline-none"
+				onChange={(e: ChangeEvent<HTMLInputElement>) =>
+					setTaskTitle(e.target.value)
+				}
 			></input>
 			<button className="w-full px-4 py-2 text-white bg-bule-500 rounded transform :hover bg-blue-500 hover:scale-95 duration-200">
 				Add Task
