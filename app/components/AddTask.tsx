03bdaafa8071e968369b22fd5680bead8fe98f0a
omuomuMG <omuomunotrice@gmail.com>
2024-05-11 15:37:58 +0900

    Add:files

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddTask() {
+	return <div>add</div>;
+}
+
+export default AddTask;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
+import AddTask from "./components/AddTask";
+import TodoList from "./components/TodoList";
 
 export default function Home() {
-	return <div></div>;
+	return (
+		<main>
+			<h1>nexjs todo</h1>
+			<div>
+				<div>
+					<AddTask />
+					<TodoList />
+				</div>
+			</div>
+		</main>
+	);
 }
