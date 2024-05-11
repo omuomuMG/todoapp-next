0c43ad556163d5b6a61019cd3d27579f55096c52
omuomuMG <omuomunotrice@gmail.com>
2024-05-11 15:56:47 +0900

    update: layout

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function AddTask() {
-	return <div>add</div>;
+	return (
+		<form className="mb-4 space-y-3">
+			<input
+				type="text"
+				className="w-full border px-4 py-2 rounded-lg focus:outline-none"
+			></input>
+			<button className="w-full px-4 py-2 text-white bg-bule-500 rounded transform :hover bg-blue-500 hover:scale-95 duration-200">
+				Add Task
+			</button>
+		</form>
+	);
 }
 
 export default AddTask;
