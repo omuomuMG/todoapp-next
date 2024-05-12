35e0d7e2c8af099201d9b3efa6a43e89445e0c71
omuomuMG <omuomunotrice@gmail.com>
2024-05-12 12:03:57 +0900

    refact: coment out

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -12,13 +12,6 @@ const TodoList = ({ todos }: TodoListProps) => {
 			{todos.map((todo) => (
 				<Todo key={todo.id} todo={todo} />
 			))}
-			{/* <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
-				<span>散歩</span>
-				<div>
-					<button className="text-green-500 mr-3">edit</button>
-					<button className="text-red-500">delete</button>
-				</div>
-			</li> */}
 		</ul>
 	);
 };
